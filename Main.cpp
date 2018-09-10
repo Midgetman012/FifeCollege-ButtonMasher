e628dd3ffb0f6ed473faaef098d72b84594f4773
Midgetman012 <43002201+Midgetman012@users.noreply.github.com>
2018-09-10 16:02:04 +0100

    Assets
    Added assets to the game

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,42 @@
 // Included Libraries
 #include <SFML/Graphics.hpp>
-
+#include <SFML/Audio.hpp>
 
 
 // entry point for the program
 int main()
 {
+	// Game setup
+
+
 	// Make a variable called gameWindow of type RenderWindow
 	sf::RenderWindow gameWindow;
 	gameWindow.create(sf::VideoMode::getDesktopMode(), "Button Masher", 
 		sf::Style::Titlebar | sf::Style::Close);
 
+	// Create button sprite
+	sf::Texture buttonTexture;
+	buttonTexture.loadFromFile("graphics/button.png");
+
+	sf::Sprite buttonSprite;
+	buttonSprite.setTexture(buttonTexture);
+
+
+	// Create music
+	sf::Music gameMusic;
+	gameMusic.openFromFile / ("audio.music.ogg");
+	gameMusic.play();
+
+
+	//Centre the sprite on screen
+
+	buttonSprite.setPosition(
+		gameWindow.getSize().x / 2 - buttonTexture.getSize().x / 2,
+		gameWindow.getSize().y / 2 - buttonTexture.getSize().y / 2);
+
+
 	// Game Loop
+	// Game setup
 	// Runs every frame until the game window is closed
 	while (gameWindow.isOpen())
 	{
@@ -32,7 +57,14 @@ int main()
 		// TODO: Update game state
 
 		// TODO: Draw graphics
+		//Clear the window to a specific colour
+		gameWindow.clear(sf::Color::Black);
 
+		//Draw everything
+		gameWindow.draw(buttonSprite);
+
+		//Display the window contents on the screen
+		gameWindow.display();
 	}
 
 	// exit point for the program
